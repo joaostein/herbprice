de9ef519b9eb3fdf698aa62a85fc8e8a56e6b462
Joao Stein <joaosteing@gmail.com>
2012-09-26 14:04:37 -0300

    Primeiro commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Herbaprice::Application.config.secret_token = 'a768fcb46d99702b4b1345523ec0dbd66147b3bbe9bf237abcf5078a1b3929045b1c6b6103d15f1e9c3797dbbe95a021c779821abf5fdb5ba1bf9e75d2714d9e'
