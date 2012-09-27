373438b539839a5001e5edb35238d6017d3f2b68
Joao Stein <joaosteing@gmail.com>
2012-09-27 17:34:20 -0300

    Limpada em arquivos desnecessarios e criado os arquivos iniciais de estilo

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,7 +47,7 @@ Herbaprice::Application.configure do
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
-  config.assets.precompile += %w( modernizr-2.5.3.min.js selectivizr-min.js )
+  config.assets.precompile += %w( modernizr-2.5.3.min.js selectivizr-min.js normalize.css)
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
